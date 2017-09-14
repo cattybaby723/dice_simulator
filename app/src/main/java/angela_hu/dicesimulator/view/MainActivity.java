e665c006fcb0f28daa23127bd2f91c03b273930d
anrou_hu <anrou_hu@hiiir.com>
2017-09-14 18:21:50 +0800

    @update prototype of MainActivity         prototype of MainViewModel

--- app/src/main/java/angela_hu/dicesimulator/view/MainActivity.java
+++ app/src/main/java/angela_hu/dicesimulator/view/MainActivity.java
@@ -1,19 +1,27 @@
 package angela_hu.dicesimulator.view;
 
 import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.List;
+
 import angela_hu.dicesimulator.R;
+import angela_hu.dicesimulator.model.Dice;
 import angela_hu.dicesimulator.viewmodel.MainViewModel;
 
-public class MainActivity extends LifecycleActivity {
-
+public class MainActivity extends LifecycleActivity implements View.OnClickListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     private EditText mNumberOfSides;
     private EditText mNumberOfDice;
@@ -31,19 +39,99 @@ public class MainActivity extends LifecycleActivity {
 
         initViews();
         initViewModel();
+        initObserver();
+        initTextWatcher();
     }
 
 
     private void initViews() {
-        mNumberOfSides = (EditText) findViewById(R.id.numberOfSides);
-        mNumberOfDice = (EditText) findViewById(R.id.numberOfDice);
-        mDiceRecyclerView = (RecyclerView) findViewById(R.id.diceRecyclerView);
-        mTotalPoint = (TextView) findViewById(R.id.totalPoint);
-        mRoll = (Button) findViewById(R.id.roll);
-    }
+        mNumberOfSides = findViewById(R.id.numberOfSides);
+        mNumberOfDice = findViewById(R.id.numberOfDice);
+        mDiceRecyclerView = findViewById(R.id.diceRecyclerView);
+        mTotalPoint = findViewById(R.id.totalPoint);
+        mRoll = findViewById(R.id.roll);
 
+        mRoll.setOnClickListener(this);
+    }
 
     private void initViewModel() {
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
     }
+
+
+    private void initObserver() {
+        mViewModel.getDices().observe(this, new Observer<List<Dice>>() {
+            @Override
+            public void onChanged(@Nullable List<Dice> dices) {
+                refreshDices();
+            }
+        });
+
+        mViewModel.getTotalPoint().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer integer) {
+                if (integer == null) return;
+                mTotalPoint.setText(integer);
+            }
+        });
+    }
+
+    private void refreshDices() {
+        Log.d(TAG, "refreshDices()");
+    }
+
+
+    private void initTextWatcher() {
+        mNumberOfSides.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int numberOfSides = Integer.valueOf(editable.toString());
+                mViewModel.setNumberOfSides(numberOfSides);
+            }
+        });
+
+        mNumberOfDice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int numberOfDice = Integer.valueOf(editable.toString());
+                mViewModel.setNumberOfDice(numberOfDice);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.roll:
+                rollDice();
+                break;
+        }
+    }
+
+    private void rollDice() {
+        mViewModel.rollDice();
+    }
+
 }
--- app/src/main/java/angela_hu/dicesimulator/viewmodel/MainViewModel.java
+++ app/src/main/java/angela_hu/dicesimulator/viewmodel/MainViewModel.java
@@ -1,10 +1,62 @@
 package angela_hu.dicesimulator.viewmodel;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+
+import java.util.List;
+
+import angela_hu.dicesimulator.model.Dice;
+import angela_hu.dicesimulator.model.RandomDiceAsyncTask;
 
 /**
  * @author anrou_hu
  */
 
 public class MainViewModel extends ViewModel {
+    private final static int DEFAULT_NUMBER_OF_SIDES = 6;
+    private final static int DEFAULT_NUMBER_OF_DICE = 3;
+
+    private int mNumberOfSides = DEFAULT_NUMBER_OF_SIDES;
+    private int mNumberOfDice = DEFAULT_NUMBER_OF_DICE;
+
+    private MediatorLiveData<List<Dice>> mDices = new MediatorLiveData<>();
+    private MutableLiveData<Integer> mTotalPoint = new MutableLiveData<>();
+
+
+    public void setNumberOfSides(int numberOfSides) {
+        mNumberOfSides = numberOfSides;
+    }
+
+    public void setNumberOfDice(int numberOfDice) {
+        mNumberOfDice = numberOfDice;
+    }
+
+    public LiveData<List<Dice>> getDices() {
+        return mDices;
+    }
+
+    public LiveData<Integer> getTotalPoint() {
+        return mTotalPoint;
+    }
+
+
+    public int calculateTotalPoint() {
+        List<Dice> dices = mDices.getValue();
+        if (dices == null) return 0;
+
+        int totalPoint = 0;
+        for (Dice dice : dices) {
+            totalPoint += dice.getCurrentPoint();
+        }
+
+        return totalPoint;
+    }
+
+
+    public void rollDice() {
+        RandomDiceAsyncTask task = new RandomDiceAsyncTask(mNumberOfSides, mNumberOfDice);
+        task.execute();
+    }
 }
